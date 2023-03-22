b22c826f09484a7e308cf00982e8de20f7a6f54d
dycat <8856528+dycat@users.noreply.github.com>
2023-03-22 21:47:22 +0800

    feat: add notification

--- done/ViewController.swift
+++ done/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
@@ -20,6 +21,42 @@ class ViewController: UIViewController {
         
         self.navigationItem.rightBarButtonItem = button
         view.backgroundColor = .systemBackground
+        
+        grandAuthorization()
+        
+        let content = setUpNotificationContent()
+        
+        var dateComponents = DateComponents()
+        dateComponents.hour = 21
+        dateComponents.minute = 39
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        
+        let notificationCenter = UNUserNotificationCenter.current()
+        notificationCenter.add(request) { err in
+            if let err = err {
+                print("Error scheduling notification: \(err.localizedDescription)")
+            }
+        }
+    }
+    
+    func grandAuthorization() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound]) { granted, error in
+            if granted {
+                print("Notification Authorized!")
+            } else {
+                
+            }
+        }
+    }
+    
+    func setUpNotificationContent() -> UNMutableNotificationContent {
+        let content = UNMutableNotificationContent()
+        content.title = "Wake up!"
+        content.body = "It's time to start your day."
+        content.sound = UNNotificationSound.default
+        return content
     }
 
 
