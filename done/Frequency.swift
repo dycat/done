20f7d6b7ceda2709dfab9a8b897d958e460ba0e0
dycat <8856528+dycat@users.noreply.github.com>
2023-03-29 21:31:57 +0800

    feat: add frequency enum

---
+++ done/Frequency.swift
@@ -0,0 +1,12 @@
+//
+//  Frequency.swift
+//  done
+//
+//  Created by yang on 29/3/2023.
+//
+
+import Foundation
+
+enum Frequency {
+    case daily, weekly, monthly
+}
---
+++ done/Week.swift
@@ -0,0 +1,12 @@
+//
+//  Week.swift
+//  done
+//
+//  Created by yang on 29/3/2023.
+//
+
+import Foundation
+
+enum Week {
+    case mon, feb, wed, thr, fri, sar, sun
+}
